309533987666223e29539488018945cc36625abc
alevya <alexv47@mail.ru>
2016-06-23 16:04:21 +0300

    edit

--- function.cpp
+++ function.cpp
@@ -76,7 +76,8 @@ void pipe_cmd(char** cmd1, char** cmd2){
 	   pid_t pid1, pid2;
 
            // child process #1
-           if (pid1 = fork() == 0) {
+	   	   pid1 = fork();
+           if (pid1 == 0) {
                 dup2(fds[1], 1);
 		close(fds[0]);
 		close(fds[1]);
